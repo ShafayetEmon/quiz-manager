c15b3f5b904a268a7d6091dbe3d33393fb046c7c
Shafayet Jamil <shafayetemon88@gmail.com>
2023-05-25 13:35:33 +0600

    JWT token added for API Authentication

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [QuizModule, ConfigModule.forRoot({ isGlobal:true }), TypeOrmModule.forRootAsync(typeOrmConfigAsync), UserModule],
+  imports: [
+    QuizModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
