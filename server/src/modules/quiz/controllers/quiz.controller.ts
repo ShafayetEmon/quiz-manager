514b25c60a5c40d250fbc7ab003e1ba38f8ecada
Shafayet Jamil <shafayetemon88@gmail.com>
2023-05-25 18:22:22 +0600

    Pagination added

--- server/src/modules/quiz/controllers/quiz.controller.ts
+++ server/src/modules/quiz/controllers/quiz.controller.ts
@@ -1,33 +1,45 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   Param,
   ParseIntPipe,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { createQuizDto } from '../dto/createQuiz.dto';
 import { QuizService } from '../services/quiz.service';
 import { Quiz } from '../entities/quiz.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Pagination } from 'nestjs-typeorm-paginate';
 
 @ApiTags('Quiz')
 @Controller('quiz')
 export class QuizController {
   constructor(private quizService: QuizService) {}
   @Get('/')
-  async getAllQuiz(): Promise<Quiz[]> {
-    return await this.quizService.getAllQuiz();
+  async getAllQuiz(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+  ): Promise<Pagination<Quiz>> {
+    limit = limit > 100 ? 100 : limit;
+    return await this.quizService.paginate({
+      page,
+      limit,
+    });
   }
 
   @Get('/:id')
+  @ApiOkResponse({ description: 'Get a quiz by id' })
   async getQuizById(@Param('id', ParseIntPipe) id: number): Promise<Quiz> {
     return await this.quizService.getQuizById(id);
   }
 
   @Post('/create')
+  @ApiCreatedResponse({ description: 'The quiz got created', type: Quiz })
   @UsePipes(ValidationPipe)
   async createQuiz(@Body() quizData: createQuizDto) {
     return await this.quizService.createNewQuiz(quizData);
--- server/src/modules/quiz/services/quiz.service.ts
+++ server/src/modules/quiz/services/quiz.service.ts
@@ -1,33 +1,42 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { createQuizDto } from "../dto/createQuiz.dto";
-import { Quiz } from "../entities/quiz.entity";
-import { Repository } from "typeorm";
-import { Question } from "../entities/question.entity";
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { createQuizDto } from '../dto/createQuiz.dto';
+import { Quiz } from '../entities/quiz.entity';
+import { Repository } from 'typeorm';
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
-export class QuizService{
+export class QuizService {
   constructor(
     @InjectRepository(Quiz) private quizRepository: Repository<Quiz>,
-  ){}
-  
-  async getAllQuiz():Promise<Quiz[]>{
+  ) {}
+
+  async getAllQuiz(): Promise<Quiz[]> {
     return await this.quizRepository
-    .createQueryBuilder('q')
-    .leftJoinAndSelect('q.questions', 'qt')
-    .leftJoinAndSelect('qt.options', 'o')
-    .getMany();
+      .createQueryBuilder('q')
+      .leftJoinAndSelect('q.questions', 'qt')
+      .leftJoinAndSelect('qt.options', 'o')
+      .getMany();
+  }
+  async paginate(options: IPaginationOptions): Promise<Pagination<Quiz>> {
+    const queryBuilder = this.quizRepository.createQueryBuilder('q');
+    queryBuilder.orderBy('q.id', 'DESC');
+
+    return paginate<Quiz>(queryBuilder, options);
   }
 
-  async getQuizById(id: number): Promise<Quiz>{
+  async getQuizById(id: number): Promise<Quiz> {
     return await this.quizRepository.findOne({
-      where: {id},
-      relations:['questions', 'questions.options'],
+      where: { id },
+      relations: ['questions', 'questions.options'],
     });
   }
 
-  async createNewQuiz(quiz: createQuizDto){
+  async createNewQuiz(quiz: createQuizDto) {
     return await this.quizRepository.save(quiz);
   }
 }
